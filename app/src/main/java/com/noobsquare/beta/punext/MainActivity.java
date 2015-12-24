26af4ebbfffd89a9f4ba432465af634a56e12446
Jeevan <jeevu.art@gmail.com>
2015-12-24 18:42:17 +0530

    scrollable tab bug

--- app/src/main/java/com/noobsquare/beta/punext/MainActivity.java
+++ app/src/main/java/com/noobsquare/beta/punext/MainActivity.java
@@ -1,28 +1,49 @@
 package com.noobsquare.beta.punext;
 
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.NavUtils.*;
 import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.app.Fragment;
+//import android.app.Fragment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import it.neokree.materialtabs.MaterialTab;
+import it.neokree.materialtabs.MaterialTabHost;
+import it.neokree.materialtabs.MaterialTabListener;
+import tabs.SlidingTabLayout;
 
 
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MaterialTabListener{
 
     private android.support.v7.widget.Toolbar toolbar;
+    private ViewPager mPager;
+    private ViewPager viewPager;
+    private SlidingTabLayout mTabs;
+    private TextView textView;
+    private MaterialTabHost tabHost;
+    private ViewPageAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_appbar);
+        setContentView(R.layout.activity_using_tab_lib);
 
         toolbar = (android.support.v7.widget.Toolbar)findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
@@ -31,7 +52,34 @@ public class MainActivity extends AppCompatActivity {
         NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
 
-        drawerFragment.setup(R.id.fragment_navigation_drawer,(DrawerLayout)findViewById(R.id.drawer_layout),toolbar);
+        drawerFragment.setup(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+
+       /* mPager = (ViewPager) findViewById(R.id.pager);// sliding tabs wic didiin work
+        mPager.setAdapter(new MypagerAdapter(getSupportFragmentManager()));
+        mTabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        mTabs.setViewPager(mPager);*/
+
+        //using lib
+        tabHost = (MaterialTabHost)findViewById(R.id.materialTabHost);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        adapter = new ViewPageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+            @Override
+            public void onPageSelected(int position) {
+                tabHost.setSelectedNavigationItem(position);
+            }
+        });
+        for(int i = 0;i < adapter.getCount();i++){
+            tabHost.addTab(tabHost.newTab()
+                    .setIcon(adapter.getIcon(i))
+                                    .setTabListener(this));
+           /* MaterialTab materialTab = tabHost.newTab();
+            materialTab.setText("Test tab");
+            materialTab.setTabListener(this);
+            tabHost.addTab(materialTab);*/
+        }
     }
 
 
@@ -63,4 +111,75 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onTabSelected(MaterialTab materialTab) {
+        viewPager.setCurrentItem(materialTab.getPosition());
+    }
+
+    @Override
+    public void onTabReselected(MaterialTab materialTab) {
+
+    }
+
+    @Override
+    public void onTabUnselected(MaterialTab materialTab) {
+
+    }
+
+    private class ViewPageAdapter extends FragmentPagerAdapter{
+
+
+        int icons[] = {R.drawable.ic_action_articles,R.drawable.ic_action_home,R.drawable.ic_action_personal};
+        public ViewPageAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return MyFragment.getInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return getResources().getStringArray(R.array.tabs)[position];
+        }
+        private Drawable getIcon(int position){
+            return getResources().getDrawable(icons[position]);
+        }
+    }
+
+/*
+    class MypagerAdapter extends FragmentPagerAdapter{
+
+        String[] tabs;
+        public MypagerAdapter(FragmentManager fm) {
+            super(fm);
+            tabs = getResources().getStringArray(R.array.tabs);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            MyFragment myFragment = new MyFragment();
+            myFragment.getInstance(position);
+            return myFragment;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabs[position];
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+    }*/
+
+
+
 }
--- app/src/main/java/com/noobsquare/beta/punext/NavigationDrawerFragment.java
+++ app/src/main/java/com/noobsquare/beta/punext/NavigationDrawerFragment.java
@@ -13,9 +13,12 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -63,12 +66,24 @@ public class NavigationDrawerFragment extends Fragment{// implements VivzAdapter
         //adapter.setClickListener(this); //method 2
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getActivity(), recyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Toast.makeText(getActivity(),"onClick"+position,Toast.LENGTH_SHORT).show();;
+
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+                Toast.makeText(getActivity(),"onLongClick"+position,Toast.LENGTH_SHORT).show();;
+            }
+        }));
         return layout;
     }
 
     public static List<Information> getData(){
         List<Information> data = new ArrayList<>();
-        int[] icons = {R.drawable.pen,R.drawable.pen,R.drawable.pen,R.drawable.ic_launcher};
+        int[] icons = {R.drawable.ic_action_personal,R.drawable.ic_action_home,R.drawable.ic_action_articles,R.drawable.pen};
         String[] titles = {"blah","blue","yo","bo"};
         for (int i =0;i<100;i++){
             Information current =  new Information();
@@ -141,4 +156,54 @@ public class NavigationDrawerFragment extends Fragment{// implements VivzAdapter
     public void itermClicked(View view, int position) {
         startActivity(new Intent(getActivity(),Subactivity.class));
     }*/
+
+    class RecyclerTouchListener implements RecyclerView.OnItemTouchListener{
+
+        private GestureDetector gestureDetector;
+        private ClickListener clickListener;
+        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final ClickListener clickListener){
+
+            this.clickListener = clickListener;
+            gestureDetector = new GestureDetector(context,new GestureDetector.SimpleOnGestureListener(){
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    //return super.onSingleTapUp(e);
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+
+                    View child = recyclerView.findChildViewUnder(e.getX(),e.getY());
+                    if(child != null && clickListener != null){
+                        clickListener.onLongClick(child,recyclerView.getChildPosition(child));
+                    }
+                    super.onLongPress(e);
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+            View child = rv.findChildViewUnder(e.getX(),e.getY());
+            if(child != null && clickListener != null && gestureDetector.onTouchEvent(e)){
+                clickListener.onClick(child, rv.getChildPosition(child));
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
+    }
+    public static interface ClickListener{
+        public void onClick(View view,int position);
+        public void onLongClick(View view, int position);
+    }
 }
